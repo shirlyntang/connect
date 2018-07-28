e79aee774aee313e04db4feef5a051b4bdfb0ab7
Shirlyn Tang <shirlyntang@gmail.com>
2018-07-28 16:12:49 -0700

    changes made

--- connect.py
+++ connect.py
@@ -16,25 +16,28 @@ def home():
 @app.route("/create_profile", methods=["POST"]) # page where user fills out form
 def get_user_info():
 
-	bool found_match = False
+	found_match = False
 	key = '';
-	fields = ['name', 'age', 'school', 'hobbies', 'animal', 'food', 'phone_number', 'picture'] #can add more
+	fields = ['username', 'name', 'age', 'school', 'hobbies', 'animals', 'foods', 'phone_number', 'picture'] #can add more
 
 	user_dict = {} # creates empty dictionary
 
 	for category in fields: # adds answers to all fields into dictionary
 
-		if category == 'hobbies': # can add other fields to this
+		if category == 'username':
+			user_dict['_id'] = request.form[category]
+
+		elif category == 'hobbies' or category == 'foods' or category == 'animals':
 			user_dict[category] = [word.strip() for word in request.form[category].split(',')]
 
 		elif category == 'phone_number':
 			number = request.form[category].translate(None, ', ')
-			if len(number) == 11 && number[0] = '1':
+			if len(number) == 11 and number[0] == '1':
 				number = number[1:]
 			number = '(' + number[0:3] + ')' + number[3:6] + '-' + number[6:]
 			user_dict[category] = number
 
-		elif category == 'picture'
+		elif category == 'picture':
 			user_dict[category] = get_image_list(request.form[category])
 
 		else:
@@ -42,23 +45,25 @@ def get_user_info():
 
 	key = insert_user(user_dict)
 	found_match = match(key)
-	
-	if found_match:
-		return redirect ("/success")
-	return redirect("/found")
 
-@app.route("/success") # found matches
+	if found_match:
+		return redirect ("/matches")
+
+@app.route("/matches") # found matches
 def success():
-    return render_template("success.html")
+	#input info of matches to html page
+    return render_template("matches.html")
 
 def get_image_list(pic_name):
 	#insert olivia's code
+	return
 
 def insert_user(user_dict):
 	db.userinfo.insert(user_dict)
-	return user_key
+	return user_dict['_id']
 
 def match(user):
 	#insert tiff's algorithm thing
+	return
 
 app.run( debug = True )
