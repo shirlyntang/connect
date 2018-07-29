d287c55fa1ce1b392c940f2a158c2b30e5c2ccab
Shirlyn Tang <shirlyntang@gmail.com>
2018-07-28 18:39:25 -0700

    errors :(

--- connect.py
+++ connect.py
@@ -20,60 +20,65 @@ def form_func():
 @app.route("/create_profile", methods=["POST"]) # page where user fills out form
 def get_user_info():
 
-	found_match = False
-	key = '';
-	fields = ['username', 'name', 'age', 'school', 'hobbies', 'animals', 'foods', 'phone_number', 'pic'] #can add more
+    found_match = False
+    key = '';
+    fields = ['username', 'name', 'age', 'school', 'hobbies', 'animals', 'foods', 'phone_number', 'pic'] #can add more
 
-	user_dict = {} # creates empty dictionary
+    user_dict = {} # creates empty dictionary
 
-	for category in fields: # adds answers to all fields into dictionary
+    for category in fields: # adds answers to all fields into dictionary
 
-		if category == 'name':
-			request.form[category] = request.form[category].title() # makes first letter of each word uppercase
-		else:
-			request.form[category] = request.form[category].lower()
+        if category == 'username':
+            user_dict['_id'] = request.form[category]
 
-		if category == 'username':
-			user_dict['_id'] = request.form[category]
+        elif category == 'hobbies' or category == 'foods' or category == 'animals':
+            user_dict[category] = [word.strip() for word in request.form[category].split(',')]
 
-		elif category == 'hobbies' or category == 'foods' or category == 'animals':
-			user_dict[category] = [word.strip() for word in request.form[category].split(',')]
+        elif category == 'phone_number':
+            number = request.form[category].replace('()', '')
+            if len(number) == 11 and number[0] == '1':
+                number = number[1:]
+            number = '(' + number[0:3] + ')' + number[3:6] + '-' + number[6:]
+            user_dict[category] = number
 
-		elif category == 'phone_number':
-			number = request.form[category].replace('()', '')
-			if len(number) == 11 and number[0] == '1':
-				number = number[1:]
-			number = '(' + number[0:3] + ')' + number[3:6] + '-' + number[6:]
-			user_dict[category] = number
+        elif category == 'pic':
+            user_dict[category] = get_image_list(request.form[category])
 
-		elif category == 'pic':
-			user_dict[category] = get_image_list(request.form[category])
+        else:
+            user_dict[category] = request.form[category]
 
-		else:
-			user_dict[category] = request.form[category]
+        if category == 'name':
+            user_dict[category] = user_dict[category].title() # makes first letter of each word uppercase
+        elif category == 'username' or category == 'pic':
+            pass
+        elif category == 'hobbies' or category == 'foods' or category == 'animals':
+            for word in user_dict[category]:
+                word = word.lower()
+        else:
+            user_dict[category] = user_dict[category].lower()
 
-	user_dict['other_matches'] = [];
+    user_dict['other_matches'] = [];
 
-	print (user_dict)
+    print (user_dict)
 
-	key = insert_user(user_dict)
-	found_match = match(key)
+    key = insert_user(user_dict)
+    found_match = match(key)
 
-	if found_match:
-		return redirect ("/matches")
+    if found_match:
+        return redirect ("/matches")
 
 @app.route("/matches") # found matches
 def success():
-	#input info of matches to html page
+    #input info of matches to html page
     return render_template("matches.html") # insert user fields as a second parameter
 
 def get_image_list(pic_name):
-	#insert olivia's code
-	return
+    #insert olivia's code
+    return ['sample']
 
 def insert_user(user_dict):
-	db.userinfo.insert(user_dict)
-	return user_dict['_id']
+    db.userinfo.insert(user_dict)
+    return user_dict['_id']
 
 def compare(person,potential):
     points = 0
@@ -85,29 +90,29 @@ def compare(person,potential):
 
 def similar(person,all_user_data):
 
-	similarity_points = dict()
+    similarity_points = dict()
 
     for potential in all_user_data:
-        if potential['id'] == person['id'] or person['id'] in potential["other_matches"]:
-            similarity_points[potential['id']]=-1
+        if potential['_id'] == person['_id'] or person['_id'] in potential["other_matches"]:
+            similarity_points[potential['_id']]=-1
         else:
             #compare age. the older the age gap the better.
-            similarity_points[potential['id']]=similarity_points.get(potential['id'],0) + math.fabs(int(person['age'])-int(potential['age']))
+            similarity_points[potential['_id']]=similarity_points.get(potential['_id'],0) + math.fabs(int(person['age'])-int(potential['age']))
 
             #compare school. potentially implement geographic location / other similarity.
             if(person['school'].lower() == potential['school'].lower()):
-                similarity_points[potential['id']]=similarity_points.get(potential['id'],0)+1
+                similarity_points[potential['_id']]=similarity_points.get(potential['_id'],0)+1
 
             #compare hobbies
-            similarity_points[potential['id']]=similarity_points.get(potential['id'],0)+compare(person['hobbies'],potential['hobbies'])
+            similarity_points[potential['_id']]=similarity_points.get(potential['_id'],0)+compare(person['hobbies'],potential['hobbies'])
 
             #compare animal
-            similarity_points[potential['id']]=similarity_points.get(potential['id'],0)+compare(person['animals'],potential['animals'])
+            similarity_points[potential['_id']]=similarity_points.get(potential['_id'],0)+compare(person['animals'],potential['animals'])
 
             #compare food
-            similarity_points[potential['id']]=similarity_points.get(potential['id'],0)+compare(person['foods'],potential['foods'])
+            similarity_points[potential['_id']]=similarity_points.get(potential['_id'],0)+compare(person['foods'],potential['foods'])
 
-            #similarity_points{people['id'],similarity_points.get(people['id']) + get_picture_similarity}
+            #similarity_points{people['_id'],similarity_points.get(people['_id']) + get_picture_similarity}
     
     highest = 0
     match = ""
@@ -122,12 +127,16 @@ def match(user_key):
 
     all_user_data =  db.userinfo.find()
 
-    user = db.userinfo.find({'_id': user_key})
+    print (all_user_data)
+
+    user = db.userinfo.find_one({'_id': user_key})
 
     match = similar(user,all_user_data)
 
     for person in all_user_data:
-        if match == person['id']:
+        if match == person['_id']:
             user['other_matches'].append(match)
+            return True
+    return False
 
 app.run( debug = True )
